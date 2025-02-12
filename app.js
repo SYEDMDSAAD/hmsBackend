7ef401804946f7b4b1662773a8ad9f86b63f7882
SYEDMDSAAD <134770714+SYEDMDSAAD@users.noreply.github.com>
2025-02-12 19:28:22 +0530

    finale

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
